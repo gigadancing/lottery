73c5233921375f977e49dd09799a8cf2a466488b
ssuchichan <ssuchichan@163.com>
2018-12-14 10:02:09 +0800

    添加同步锁保操作全局变量userLists时证线程安全；添加一些注释。

--- annual_meeting_draws/annual_meeting_draws.go
+++ annual_meeting_draws/annual_meeting_draws.go
@@ -6,10 +6,15 @@ import (
 	"github.com/kataras/iris/mvc"
 	"math/rand"
 	"strings"
+	"sync"
 	"time"
 )
 
+// 参与抽奖的用户
 var userLists []string
+
+// 同步锁
+var mu sync.Mutex
 
 type lotteryController struct {
 	Ctx iris.Context
@@ -25,6 +30,7 @@ func main() {
 	app := newApp()
 	// userLists = make([]string, 0)
 	userLists = []string{}
+	mu = sync.Mutex{}
 	app.Run(iris.Addr(":8080"))
 }
 
@@ -34,9 +40,12 @@ func (c *lotteryController) Get() string {
 }
 
 // POST http://localhost:8080/import
+// 导入用户
 func (c *lotteryController) PostImport() string {
 	strUsers := c.Ctx.FormValue("users")
 	users := strings.Split(strUsers, ",")
+	mu.Lock()
+	defer mu.Unlock()
 	count1 := len(userLists)
 	for _, u := range users {
 		u = strings.TrimSpace(u)
@@ -48,13 +57,19 @@ func (c *lotteryController) PostImport() string {
 	return fmt.Sprintf("total users: %d, success imported users: %d\n", count2, count2-count1)
 }
 
-//GET http://localhost:8080/lucky
+// GET http://localhost:8080/lucky
+// 抽奖
 func (c *lotteryController) GetLucky() string {
+	mu.Lock()
+	defer mu.Unlock()
 	count := len(userLists)
 	if count > 1 {
+		// 随机数种子
 		seed := time.Now().UnixNano()
+		// 产生[0, count)的随机数
 		index := rand.New(rand.NewSource(seed)).Int31n(int32(count))
 		user := userLists[index]
+		// 将中奖的用户删除
 		userLists = append(userLists[0:index], userLists[index+1:]...)
 		return fmt.Sprintf("the lucky: %s, remaining users: %d\n", user, count-1)
 	} else if count == 1 {
